8e5be0165ed65163edc8f620940c59b61f8833ec
baebumsoo <baebeomsu77@gmail.com>
2022-11-22 18:38:23 +0900

    Feat(bumsoo/client): api 관리를 위한 추상화

---
+++ client/apis/base.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+import { HTTP_METHODS } from '@consts/net';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_KEY,
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+const createApiMethod =
+  (_axiosInstace: AxiosInstance, methodType: Method) =>
+  (config: AxiosRequestConfig) => {
+    return _axiosInstace({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
