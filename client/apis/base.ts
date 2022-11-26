2655e184eaa690833ac2fa801be8c5d8b257c2cc
baebumsoo <baebeomsu77@gmail.com>
2022-11-26 17:29:54 +0900

    Modify(bumsoo/client): 쿠키 공유를 위해 axios설정 변경

--- client/apis/base.ts
+++ client/apis/base.ts
@@ -5,6 +5,7 @@ const axiosInstance: AxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_KEY,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 });
 
 const createApiMethod =
