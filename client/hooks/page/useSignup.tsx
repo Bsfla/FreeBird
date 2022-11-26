7e8623d8566ec97c431a261dea4e005607392984
baebumsoo <baebeomsu77@gmail.com>
2022-11-26 15:58:54 +0900

    Feat(bumsoo/client): 회원 가입 성공 시 메세지 띄워주고 로그인 페이지로 라우팅 구현

--- client/hooks/page/useSignup.tsx
+++ client/hooks/page/useSignup.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { useInput } from '@hooks/common';
 import { UserFormType } from '@lib/types';
 import { signUp } from '@apis/user';
 import { AxiosError } from 'axios';
+import { LOGIN_PAGE } from '@consts/route';
 
 const useSignup = () => {
   const { form, handleChangeInput } = useInput<UserFormType>({
@@ -11,6 +13,7 @@ const useSignup = () => {
     password: '',
     passwordconfirm: '',
   });
+  const router = useRouter();
   const { email, nickname, password, passwordconfirm } = form;
 
   const isDisabled =
@@ -25,6 +28,8 @@ const useSignup = () => {
 
     try {
       await signUp(form);
+      alert('회원가입에 성공했습니다');
+      router.push(LOGIN_PAGE);
     } catch (err) {
       if (err instanceof AxiosError)
         return alert('이미 등록되어 있는 아이디입니다');
