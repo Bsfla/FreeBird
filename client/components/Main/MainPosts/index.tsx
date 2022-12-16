ab6de7d858498a002e24f03669bb5a162bca600f
baebumsoo <baebeomsu77@gmail.com>
2022-12-16 20:29:22 +0900

    Feat(bumsoo/client): 무한스크롤 로직 재사용을 위해 커스텀 훅 구현

--- client/components/Main/MainPosts/index.tsx
+++ client/components/Main/MainPosts/index.tsx
@@ -1,35 +1,19 @@
-import React, { useEffect } from 'react';
-import { useInfiniteQuery } from 'react-query';
-import { useInView } from 'react-intersection-observer';
+import React from 'react';
 import { getPosts } from '@apis/post';
 import { PostList } from '@components/common';
 import { PostType } from '@lib/types';
-import { AxiosError } from 'axios';
+import { useInfiniteScroll } from '@hooks/common';
+import { queryKeys } from '@consts/queryKeys';
 
 const MainPosts = () => {
-  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<
-    PostType[],
-    AxiosError,
-    PostType[]
-  >('posts', ({ pageParam = '' }) => getPosts(pageParam), {
-    getNextPageParam: (lastPage) => {
-      return lastPage?.[lastPage.length - 1]?.id;
-    },
-    staleTime: Infinity,
-    refetchOnWindowFocus: false,
-  });
-  const [ref, inView] = useInView();
-  const posts = data?.pages.flat();
-  const isLast = data && data?.pages[data.pages.length - 1]?.length < 10;
-
-
-  useEffect(() => {
-    if (!isLast && hasNextPage && inView) fetchNextPage();
-  }, [inView]);
+  const { ref, resultData: posts } = useInfiniteScroll<PostType[]>(
+    queryKeys.posts,
+    getPosts
+  );
 
   return (
     <>
-      {data && <PostList posts={posts} />}
+      {posts && <PostList posts={posts} />}
       <div ref={ref} />
     </>
   );
---
+++ client/hooks/common/useInfiniteScroll.ts
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { QueryKey, useInfiniteQuery } from 'react-query';
+import { useInView } from 'react-intersection-observer';
+import { FetchingDataType } from '@lib/types';
+import { AxiosError } from 'axios';
+
+const useInfiniteScroll = <T extends FetchingDataType[]>(
+  queryKey: QueryKey,
+  api: (lastId?: number) => Promise<T>
+) => {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<
+    T,
+    AxiosError,
+    T
+  >(queryKey, ({ pageParam = '' }) => api(pageParam), {
+    getNextPageParam: (lastPage) => {
+      return lastPage?.[lastPage.length - 1]?.id;
+    },
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  });
+  const [ref, inView] = useInView();
+  const resultData = data?.pages.flat();
+  const isLast = data && data?.pages[data.pages.length - 1]?.length < 10;
+
+  useEffect(() => {
+    if (!isLast && hasNextPage && inView) fetchNextPage();
+  }, [inView]);
+
+  return { ref, resultData };
+};
+
+export default useInfiniteScroll;
