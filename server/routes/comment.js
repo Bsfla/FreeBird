d499d43ac4ad591469fea1029e38da68b24dcd23
baebumsoo <baebeomsu77@gmail.com>
2023-01-07 23:42:55 +0900

    Feat(bumsoo/server): 댓글 불러오기 api구현

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -4,7 +4,7 @@ const { isLoggedIn } = require("./middleware");
 
 const router = express.Router();
 
-router.post("/:postId/comment", async (req, res, next) => {
+router.post("/:postId", async (req, res, next) => {
   try {
     const post = await Post.findOne({
       where: {
@@ -21,6 +21,34 @@ router.post("/:postId/comment", async (req, res, next) => {
     });
 
     res.status(201).send("댓글을 생성했습니다");
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:postId", async (req, res, next) => {
+  try {
+    const post = await Post.findOne({
+      where: {
+        id: req.params.postId,
+      },
+    });
+
+    if (!post) return res.status(403).send("존재하지 않는 게시물입니다");
+
+    const comments = await Comment.findAll({
+      where: {
+        PostId: req.params.postId,
+      },
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+        },
+      ],
+    });
+
+    res.status(200).json(comments);
   } catch (err) {
     next(err);
   }
