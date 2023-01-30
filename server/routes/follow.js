75cbf3fa8f70addad36e61ba86283e15860592b0
baebumsoo <baebeomsu77@gmail.com>
2023-01-30 17:45:34 +0900

    Feat(bumsoo/server): 팔로잉  목록 불러오는 api 구현

--- server/routes/follow.js
+++ server/routes/follow.js
@@ -6,7 +6,7 @@ const db = require("../models");
 const { isLoggedIn } = require("./middleware");
 const router = express.Router();
 
-router.get("/:userId/follower", async (req, res, next) => {
+router.get("/:userId/followers", async (req, res, next) => {
   try {
     const user = await User.findOne({
       where: {
@@ -25,6 +25,43 @@ router.get("/:userId/follower", async (req, res, next) => {
     }
 
     const followers = await user.getFollowers({
+      where,
+      limit: 10,
+      attributes: ["id", "nickname"],
+      include: [
+        {
+          model: Image,
+          as: "ProfileImage",
+          attributes: ["src"],
+        },
+      ],
+    });
+    res.status(200).json(followers);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/:userId/followings", async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        id: req.params.userId,
+      },
+    });
+
+    if (!user) return res.status(404).send("유저가 존재하지 않습니다");
+
+    const where = {};
+
+    const lastId = parseInt(req.query.lastId, 10);
+
+    if (lastId) {
+      where.id = { [Op.lt]: lastId };
+    }
+
+    const followers = await user.getFollowings({
       where,
       limit: 10,
       attributes: ["id", "nickname"],
