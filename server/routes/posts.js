337211776a5cd26609d57e43acc1d672f6af58db
baebumsoo <baebeomsu77@gmail.com>
2023-02-13 21:20:34 +0900

    Feat(bumsoo/server): 해쉬태그 검색 api 구현

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -79,4 +79,79 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:hashtag", async (req, res, next) => {
+  try {
+    const where = {};
+    const lastId = parseInt(req.query.lastId, 10);
+
+    if (lastId) {
+      where.id = { [Op.lt]: lastId };
+    }
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [["createdAt", "DESC"]],
+      include: [
+        {
+          model: User,
+          attributes: ["id", "nickname"],
+          include: [
+            {
+              model: Image,
+              as: "ProfileImage",
+              attributes: ["src"],
+            },
+          ],
+        },
+        {
+          model: Image,
+          attributes: ["src"],
+        },
+        {
+          model: Hashtag,
+          where: { name: req.params.hashtag },
+          attributes: ["name"],
+        },
+        {
+          model: User,
+          as: "Likers",
+          attributes: ["id"],
+        },
+        {
+          model: Post,
+          as: "Retweet",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            {
+              model: Image,
+              attributes: ["src"],
+            },
+          ],
+        },
+        {
+          model: Post,
+          as: "Retweets",
+          include: [
+            {
+              model: User,
+              attributes: ["id", "nickname"],
+            },
+            {
+              model: Image,
+              attributes: ["src"],
+            },
+          ],
+        },
+      ],
+    });
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
 module.exports = router;
