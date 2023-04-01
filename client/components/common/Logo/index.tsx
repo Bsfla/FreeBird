640a46b6b7a683896fddb85a2128e91081ca6bca
baebumsoo <baebeomsu77@gmail.com>
2023-04-01 22:00:05 +0900

    Modify(bumsoo/client): 로고 수정

--- client/components/common/Logo/index.tsx
+++ client/components/common/Logo/index.tsx
@@ -3,11 +3,12 @@ import { LogoWrapper } from './style';
 import LogoIcon from '@assets/svg/birdicon.svg';
 import Link from 'next/link';
 import { MAIN_PAGE } from '@consts/route';
+import { FaKiwiBird } from 'react-icons/fa';
 
 const Logo = () => {
   return (
     <LogoWrapper>
-      <LogoIcon />
+      <FaKiwiBird size={5} />
       <Link href={MAIN_PAGE}>
         <span>FreeBird</span>
       </Link>
