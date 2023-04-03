e25dfd1b7f0f3e9e707434a3eec121491d056251
baebumsoo <baebeomsu77@gmail.com>
2023-04-03 21:48:46 +0900

    Modify(bumsoo/server): 수정

--- server/routes/post.js
+++ server/routes/post.js
@@ -18,7 +18,7 @@ try {
 
 AWS.config.update({
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
-  secretAccessKey: process.env.S3.SECRET_ACCESS_KEY,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
   region: "ap-northeast-2",
 });
 
