fe03e1a3bc48be095d6ce6620b07c2882e558513
baebumsoo <baebeomsu77@gmail.com>
2023-04-03 19:07:16 +0900

    Modify(bumsoo/server): cors수정

--- server/app.js
+++ server/app.js
@@ -30,25 +30,30 @@ db.sequelize
   .catch(console.error);
 passportConfig();
 
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
-
 if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   app.use(hpp());
   app.use(helmet());
+  app.use(
+    cors({
+      origin: "http://freebirdapp.site:3060",
+      credentials: true,
+    })
+  );
 } else {
   app.use(morgan("dev"));
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
 }
 
 app.use("/", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser("poketbirdsecret"));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(
   session({
     saveUnitialized: false,
@@ -56,6 +61,7 @@ app.use(
     secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
+      secure: false,
     },
   })
 );
