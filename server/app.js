48033ed887b3d8560178311f7f993f6286fc00c5
baebumsoo <baebeomsu77@gmail.com>
2023-04-03 19:21:00 +0900

    Modify(bumsoo/client): 빌드 수정

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(helmet());
   app.use(
     cors({
-      origin: "http://freebirdapp.site:3060",
+      origin: true,
       credentials: true,
     })
   );
